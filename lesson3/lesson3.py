790e295113bc971f678a64cb4fdcf3415c88153c
Александр Беспалов <bsplv@MacBook-Air-Aleksandr.local>
2023-04-21 17:24:30 +0300

    Add the lesson3

---
+++ lesson3/lesson3.py
@@ -0,0 +1,14 @@
+class Card:
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.tank - rank
+
+    def popCard(self):
+        return self.cards.pop()
+
+    def addCard(self, card):
+        self.cars.append(card)
+
+
+cDiamondsQueen = Card(1, 12)
